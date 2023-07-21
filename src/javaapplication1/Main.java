b505cfd3096ac4214f521878129ffccd2b94cc11
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-21 17:14:22 +0300

    POLIMORPHISM

--- src/javaapplication1/Main.java
+++ src/javaapplication1/Main.java
@@ -2,18 +2,73 @@ package javaapplication1;
 
 public class Main {
 
-    // COMPOSITION
-
     public static void main(String[] args) {
-
-        Resolution resolution = new Resolution(1920, 1080);
-        Monitor monitor = new Monitor("VS197DE", "ASUS", "18.5", resolution);
-        Kasa kasa = new Kasa("Shadow Blade", "Shadow", "Tamperli Cam");
-        Anakart anakart = new Anakart("B250-PRO", "ASUS", 10, "Windows 10");
-        Bilgisayar pc = new Bilgisayar(monitor, kasa, anakart);
-
-        pc.getKasa().bilgisayariAc();
-        pc.getMonitor().monitoruKapat();
-        pc.getAnakart().isletimSistemiYukle("Ubuntu 16.04");
+        
+        // Kedi referansını üst sınıfa eşitleyebiliyoruz
+        // Buna POLYMORPHISM denir.
+        // (Kalıtım olmak durumunda)
+        // Tür dönüşümü olmadan parametre olarak yollayabiliyoruz, bu bir avantaj.
+        
+        Hayvan hayvan1 = new Hayvan();
+        hayvan1.hayvanKonusuyor();
+        
+        Hayvan hayvan2 = new Kedi("Tekir");
+        hayvan2.hayvanKonusuyor();
+        
+        // hayvan2 referansı Kedi sınıfından mıdır ?
+        if(hayvan2 instanceof Kedi){
+            System.out.println("hayvan2 Kedi referansindandir.");
+        }
     }
 }
+
+// public yazarsak dosya adıyla aynı olmak zorunda
+class Hayvan {
+    
+    private String isim;
+
+    /**
+     * @return the isim
+     */
+    public String getIsim() {
+        return isim;
+    }
+
+    /**
+     * @param isim the isim to set
+     */
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public void hayvanKonusuyor (){
+        System.out.println("Hayvan konusuyor..");
+    }
+    
+}
+ class Kedi extends Hayvan {
+     private String isim;
+     
+     public Kedi(String isim) {
+         this.isim = isim;
+     }
+
+    /**
+     * @return the isim
+     */
+    public String getIsim() {
+        return isim;
+    }
+
+    /**
+     * @param isim the isim to set
+     */
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+     
+     @Override
+     public void hayvanKonusuyor(){
+         System.out.println(this.isim + " miyavliyor..");
+     }
+ }
