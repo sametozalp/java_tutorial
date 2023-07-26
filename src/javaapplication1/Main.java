148f93f4b26bfc448f6e882deb6d29f646985a90
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-26 14:02:31 +0300

    FOREACH

--- src/javaapplication1/Main.java
+++ src/javaapplication1/Main.java
@@ -3,72 +3,12 @@ package javaapplication1;
 public class Main {
 
     public static void main(String[] args) {
-        
-        // Kedi referansını üst sınıfa eşitleyebiliyoruz
-        // Buna POLYMORPHISM denir.
-        // (Kalıtım olmak durumunda)
-        // Tür dönüşümü olmadan parametre olarak yollayabiliyoruz, bu bir avantaj.
-        
-        Hayvan hayvan1 = new Hayvan();
-        hayvan1.hayvanKonusuyor();
-        
-        Hayvan hayvan2 = new Kedi("Tekir");
-        hayvan2.hayvanKonusuyor();
-        
-        // hayvan2 referansı Kedi sınıfından mıdır ?
-        if(hayvan2 instanceof Kedi){
-            System.out.println("hayvan2 Kedi referansindandir.");
+
+        String[] array = { "elma", "armut", "kiraz" };
+
+        for (String a : array) {
+            System.out.println(a);
         }
+
     }
 }
-
-// public yazarsak dosya adıyla aynı olmak zorunda
-class Hayvan {
-    
-    private String isim;
-
-    /**
-     * @return the isim
-     */
-    public String getIsim() {
-        return isim;
-    }
-
-    /**
-     * @param isim the isim to set
-     */
-    public void setIsim(String isim) {
-        this.isim = isim;
-    }
-
-    public void hayvanKonusuyor (){
-        System.out.println("Hayvan konusuyor..");
-    }
-    
-}
- class Kedi extends Hayvan {
-     private String isim;
-     
-     public Kedi(String isim) {
-         this.isim = isim;
-     }
-
-    /**
-     * @return the isim
-     */
-    public String getIsim() {
-        return isim;
-    }
-
-    /**
-     * @param isim the isim to set
-     */
-    public void setIsim(String isim) {
-        this.isim = isim;
-    }
-     
-     @Override
-     public void hayvanKonusuyor(){
-         System.out.println(this.isim + " miyavliyor..");
-     }
- }
