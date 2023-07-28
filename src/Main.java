b5416a436564435afb126d2e236038312b0a78a2
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-28 21:29:42 +0300

    LOCAL INNER CLASSES

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
-        //STATIC INNER CLASSES
+        // LOCAL INNER CLASSES - public, static anahtar kelimelerini kullanamıyoruz.
 
-        Matematik.Alan alan = new Matematik.Alan();
+        class Alan {
+            public void daireAlan() {
+                System.out.println("daire alan fonk");
+            }
+        }
 
+        Alan alan = new Alan();
         alan.daireAlan();
-        // Matematik.Alan.daireAlan(); // eğer fonk static olsaydı
-    } 
+
+        // public veya static anahtar kelimelerini kullanamıyoruz
+        // ayrıca farklı bir fonk içerisinden de bu classa ulaşamıyoruz
+        /*
+         * public class Alane {
+         * public void daireAlan() {
+         * System.out.println("daire alan fonk");
+         * }
+         * }
+         */
+
+    }
 }
