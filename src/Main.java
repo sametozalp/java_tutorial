3aacbd9f6c9eb25f8e7f81417ef0f22002c62684
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-28 22:10:55 +0300

    ANONIM INNER CLASSES

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,82 @@
 public class Main {
     public static void main(String[] args) {
 
-        // LOCAL INNER CLASSES - public, static anahtar kelimelerini kullanamıyoruz.
+        // ANONIM INNER CLASSES
+        // interface ve abstract sınıfların metotlarına erişebilmek için anonim inner
+        // classları kullanıyoruz
+        // böylece herhangi bir class tanımlamasına gerek kalmıyor.
 
-        class Alan {
-            public void daireAlan() {
-                System.out.println("daire alan fonk");
+        IOgrenci ogrenci = new IOgrenci() {
+
+            @Override
+            public void dersCalis() {
+                System.out.println("ders calisiyorum");
             }
+
+            @Override
+            public void derseGir() {
+                System.out.println("derse giriyorum");
+            }
+
+        };
+        ogrenci.dersCalis();
+        ogrenci.derseGir();
+
+        System.out.println("*******************************");
+
+        AOgrenci aOgrenci = new AOgrenci("Samet Özalp", 171) {
+
+            @Override
+            void kayitYaptir() {
+                System.out.println("kayit yapiliyor.. " + "İsim: " + getIsim() + " Numara: " + getNumara());
+            }
+
+        };
+
+        aOgrenci.kayitYaptir();
+        aOgrenci.selamla();
+
+    }
+
+    // ABSTRACT SINIFINI STATIC YAPMAZSAK YUKARIDA KULLANAMAYIZ
+    // çünkü interface gibi değil içerisinde yazılmış metotlar bulunmakta
+    // kullanabilmek için static olarak tanımlamalıyız
+
+    public static abstract class AOgrenci {
+        private String isim;
+        private int numara;
+
+        public AOgrenci(String isim, int numara) {
+            this.isim = isim;
+            this.numara = numara;
         }
 
-        Alan alan = new Alan();
-        alan.daireAlan();
+        abstract void kayitYaptir();
 
-        // public veya static anahtar kelimelerini kullanamıyoruz
-        // ayrıca farklı bir fonk içerisinden de bu classa ulaşamıyoruz
-        /*
-         * public class Alane {
-         * public void daireAlan() {
-         * System.out.println("daire alan fonk");
-         * }
-         * }
-         */
+        public void selamla() {
+            System.out.println("Selamlar..");
+        }
 
+        public String getIsim() {
+            return isim;
+        }
+
+        public void setIsim(String isim) {
+            this.isim = isim;
+        }
+
+        public int getNumara() {
+            return numara;
+        }
+
+        public void setNumara(int numara) {
+            this.numara = numara;
+        }
+    }
+
+    public interface IOgrenci {
+        void dersCalis();
+
+        void derseGir();
     }
 }
