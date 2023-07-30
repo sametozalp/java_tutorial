ea4ce3682dc07e6196f9d8f5ddb090d8a4e5743c
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-30 22:03:50 +0300

    INTERFACEDE ÖZELLİK EKLEME

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        FinalTest f1 = new FinalTest("Ahmet");
-        FinalTest f2 = new FinalTest("Samet");
 
-        System.out.println("Obje sayısı: " + f1.objeSayisi);
-        System.out.println("Obje sayısı: " + f2.objeSayisi);
+        // INTERFACEDE ÖZELLİK EKLEME
 
-        // f2.objeSayisi = 10; bu işlem yapılamaz
+        // static olmasa bile interfacede bu şekilde değişkene erişebiliriz
+        System.out.println(IDeneme.a);
+        System.out.println(IDeneme.c);
 
-        Database database = new Database();
-        database.baglantiKur("Samet", "123456");
+        // IDeneme.a = 1; DEĞİŞKENLERE DE MÜDAHALE EDEMİYORUZ FINAL OLMASA BILE
 
+        // static olduğu için tanımlayabildik ve kullandık
+        IDeneme.deneme();
     }
 }
