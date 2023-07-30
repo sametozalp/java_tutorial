1e52bc8b8b872ad454606135e5f384dbf371785f
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-30 15:02:06 +0300

    PACKAGES

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,22 @@
+
+import org.w3c.dom.Node;
+
 public class Main {
     public static void main(String[] args) {
 
-        // GENERICS: (<E>) kullanımıyla verimlilik sağlar
-        /*
-         * Character[] charDizi = { 'J', 'A', 'V', 'A' };
-         * Integer[] integerDizi = { 1, 2, 3, 4, 5, 6 };
-         * String[] StringDizi = { "Java", "Python", "C++", "Php" };
-         * Ogrenci[] ogrenciDizi = { new Ogrenci("Samet"), new Ogrenci("Ahmet"), new
-         * Ogrenci("Mehmet") };
-         * 
-         * yazdir(charDizi);
-         * yazdir(StringDizi);
-         * yazdir(integerDizi);
-         * yazdir(ogrenciDizi);
-         * }
-         * 
-         * public static <E> void yazdir(E[] dizi) {
-         * for (E e : dizi)
-         * System.out.println(e);
-         */
+        // paketleri container görevi görür
+        // farklı classlarda aynı isimler olsa bile biz farklı paket isimleri verdiğimiz
+        // takdirde bunlar birbirine karışmıyor
 
-        Sayisal sayisal1 = new Sayisal(30, 40, 20, 30);
-        Sayisal sayisal2 = new Sayisal(25, 35, 2, 35);
-        Sayisal birinci = birinci(sayisal1, sayisal2);
+        Node node = null;
 
-        Tm tm1 = new Tm(30, 20, 40, 2);
-        Tm tm2 = new Tm(40, 10, 0, 50);
-        Tm birinci2 = birinci(tm1, tm2);
-        System.out.println("Birinci sayısal öğrencinin puanı: " + birinci.puanHesapla());
-        System.out.println("Birinci eşit ağırlık öğrencinin puanı: " + birinci2.puanHesapla());
-    }
+        // iki paketi bu şekilde dahil edip kullanamayız ancak şu şekilde yapılabilir.
+        // javax.xml.soap.Node node2 = null; (paketi bulamadı ama mantık bu)
 
-    public static <E extends Aday> E birinci(E e1, E e2) {
-        if (e1.puanHesapla() > e2.puanHesapla()) {
-            return e1;
-        } else {
-            return e2;
-        }
+        // farklı paketteki classlara erişmeki için o paketi import etmek gerekiyor
+
+        // kendi yazdığımız classı jar olarak dışarı çıkıp farklı projelere ekleyip
+        // kullanabiliyoruz
+
     }
 }
