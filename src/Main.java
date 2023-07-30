e156d899f393501065679e073e29a95004c15972
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-07-30 16:12:09 +0300

    DEĞİŞKEN KAPSAMI
    inner classtaki değişken ana classtaki fonk tarafından kullanılabilir

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-
-import org.w3c.dom.Node;
-
 public class Main {
     public static void main(String[] args) {
 
-        // paketleri container görevi görür
-        // farklı classlarda aynı isimler olsa bile biz farklı paket isimleri verdiğimiz
-        // takdirde bunlar birbirine karışmıyor
-
-        Node node = null;
-
-        // iki paketi bu şekilde dahil edip kullanamayız ancak şu şekilde yapılabilir.
-        // javax.xml.soap.Node node2 = null; (paketi bulamadı ama mantık bu)
-
-        // farklı paketteki classlara erişmeki için o paketi import etmek gerekiyor
-
-        // kendi yazdığımız classı jar olarak dışarı çıkıp farklı projelere ekleyip
-        // kullanabiliyoruz
-
+        Kapsam.Dahili dahili = new Kapsam().new Dahili();
+        dahili.degiskenKullan();
+        new Kapsam().dahiliKontrol();
     }
 }
