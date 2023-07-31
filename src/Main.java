a239a0d992d6cc68b52ab1e9ff6ec2917c270323
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-08-01 00:59:54 +0300

    COLLECTION FRAMEWORK

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        // INTERFACEDE ÖZELLİK EKLEME
-
-        // static olmasa bile interfacede bu şekilde değişkene erişebiliriz
-        System.out.println(IDeneme.a);
-        System.out.println(IDeneme.c);
-
-        // IDeneme.a = 1; DEĞİŞKENLERE DE MÜDAHALE EDEMİYORUZ FINAL OLMASA BILE
-
-        // static olduğu için tanımlayabildik ve kullandık
-        IDeneme.deneme();
+        // COLLECTION FRAMEWORK
+        // tablo önemli
+        
     }
 }
