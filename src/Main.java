d4b27e01c71d3d88528d3b84b6a7cacddcb763a5
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-08-02 13:51:09 +0300

    HASHMAP

--- src/Main.java
+++ src/Main.java
@@ -1,92 +1,30 @@
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
 
-        // SET interface ==> HashSet, LinkedHashSet, TreeSet
+        // HASHMAP
+        // Key and value
+        // Keyden sadece bir tane olabilir. birden fazla value olabilir.
+        // Elementleri ekleme sırasına göre depolamaz. (HashSet gibi)
+        // Map Collection interface'inden implement etmez.
 
-        // Set ile List interface arasındaki fark şu:
-        // List bir elementten birkaç tane depolarken Set sadece bir tane depoluyor
+        HashMap<Integer, String> hashMap = new HashMap<>();
 
-        // Elementler HashSet içerisinde ekleme sırasına göre depolanmıyor.
-        // LinkedHashSet elementleri ekleme sırasına göre depoluyor.
-        // TreeSet elementleri Tree'ye göre yerleştiriyor yani alfabetik olarak
-        // sıralanıyor.
+        hashMap.put(10, "Java");
+        hashMap.put(30, "Python");
+        hashMap.put(50, "Php");
+        hashMap.put(20, "Php");
+        hashMap.put(50, "JavaScript"); // üzerine yazar.
 
-        // HashSet'te elemanları tak diye bulabiliyoruz çünkü nerde oldukları hashing
-        // mekanizmasından dolayı bellidir. Yani ekleme, çıkarma, arama metodları sabit
-        // bir zamanda çalışır.
+        System.out.println(hashMap);
+        System.out.println(hashMap.get(50));
 
-        // TreeSet'te içerisinde ne kadar çok element olursa ekleme, çıkarma, arama
-        // metodları o kadar yavaş çalışır.
-
-        // LinkedHashSet sınıfı hashtable ile linkedlist yapısını kullanarak elementleri
-        // depolar. Bu yüzden ekleme sırasına göre depolanır. Ekleme, çıkarma, arama
-        // metodları sabit bir zamanda çalışır(HashSet gibi).
-
-        Set<String> set1 = new HashSet<>(); // Karışık depoladı
-        Set<String> set2 = new LinkedHashSet<>(); // Sırasıyla depoladı
-        Set<String> set3 = new TreeSet<>(); // Alfabetik depoladı
-
-        set1.add("Java");
-        set1.add("Python");
-        set1.add("C++");
-        set1.add("JavaScript");
-        set1.add("Php");
-
-        set2.add("Java");
-        set2.add("Python");
-        set2.add("C++");
-        set2.add("JavaScript");
-        set2.add("Php");
-
-        set3.add("Java");
-        set3.add("Python");
-        set3.add("C++");
-        set3.add("JavaScript");
-        set3.add("Php");
-        set3.add("Php"); // tekrar eklenmedi
-
-        System.out.println("******HashSet**********");
-        for (String s : set1) {
-            System.out.println(s);
+        // liste gibi alt alta yazdırmak
+        for (Map.Entry<Integer, String> entry : hashMap.entrySet()) {
+            System.out.println(entry.getKey() + " => " + entry.getValue());
         }
 
-        System.out.println("******LinkedHashSet**********");
-        for (String s : set2) {
-            System.out.println(s);
-        }
-
-        System.out.println("******TreeSet**********");
-        for (String s : set3) {
-            System.out.println(s);
-        }
-
-        /******************************************************************************* */
-        System.out.println("*****************************************");
-
-        Set<String> s1 = new HashSet<>();
-        Set<String> s2 = new HashSet<>();
-
-        s1.add("Java");
-        s1.add("C++");
-        s1.add("Python");
-        s1.add("Javascript");
-        s1.add("Php");
-
-        s2.add("Go");
-        s2.add("Java");
-        s2.add("Css");
-
-        Set<String> fark = new HashSet<>(s2); // fark kümesini s2'ye eşitler
-        System.out.println(fark.removeAll(s1)); // fark içerisinden s1'e ait olan elemanları siliyor
-        System.out.println(fark); // s2'ye ait olan özellikler tek kalıyor
-
-        Set<String> kesisim = new HashSet<>(s2);
-        System.out.println(kesisim.retainAll(s1)); // burda da iki kümenin kesişimi gerçekleşiyor
-        System.out.println(kesisim);
     }
 }
