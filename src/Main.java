5179bb97af8ba8cfc78b95b5f36437ddd6abb030
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-08-03 13:54:28 +0300

    PRIORITYQUEUE

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,92 @@
-import java.util.LinkedList;
+import java.util.PriorityQueue;
 import java.util.Queue;
+
+import javax.xml.transform.Source;
 
 public class Main {
     public static void main(String[] args) {
 
-        // QUEUE: KUYRUK (Interface)
-        // Linkedlist ve PriorityQueue bu interfaceden implement eden somut
-        // classlardır.
-        // Kuyruğa ilk giren ilk çıkar. (Stack'in zıttı.)
-        // offer add'den daha güvenli bir metot (eleman ekler).
-        // remove() => kuyruğun en başındaki eleman kuyruktan çıkar. kuyruk boşsa hata
-        // verir.
-        // poll() => kuyruğun en başındaki eleman kuyruktan çıkar. kuyruk boşsa null
-        // döner.
-        // element() => kuyruğun en başındaki elemanı döner. kuyruk boşsa hata fırlatır.
-        // peek() => kuyruğun en başındaki elemanı döner. kuyruk boşsa null döner.
+        // PriorityQueue
 
-        Queue<String> queue = new LinkedList<>();
-        queue.offer("Java");
-        queue.offer("Python");
-        queue.offer("Php");
-        queue.offer("C++");
+        // PriorityQueue, queue mantığu gibi çalışmaz. Elemanlar öncelik sırasına göre
+        // öncelik kazanıp sıraya geçiyor.
+
+        // Integerlarda öncelik: küçük sayıdan büyük sayıya.
+        // Stringlerde öncelik: sözlük sırası (alfabetik).
+
+        // add() - offer()
+        // clear()
+        // constains(Object o)
+        // peek()
+        // poll()
+        // size()
+
+        Queue<Integer> queue = new PriorityQueue<>();
+        queue.offer(5);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(100);
 
         System.out.println(queue.peek()); // kuyruğun en başındaki eleman
-        System.out.println(queue.poll()); // kuyruğun en başındaki elemanı siler
 
-        System.out.println("***************************");
+        // BİZ BUNLARI BASTIRIRKEN ÖNCELİK SIRASINA GÖRE GÖREMİYORUZ FAKAT ÖNCELİK
+        // SIRASINA GÖRE SIRALI OLDUĞUNU BİLMEMİZ GEREKİYOR.
 
-        for (String s : queue) {
-            System.out.println(s);
+        while (!queue.isEmpty()) {
+            System.out.println("Eleman çıkarılıyor: " + queue.poll());
         }
 
-        
+        // KENDİ OBJENİ SIRALA
+        Queue<Player> players = new PriorityQueue<>();
+        players.offer(new Player("Samet", 5));
+        players.offer(new Player("Mehmet", 1));
+        players.offer(new Player("Ahmet", 100));
+
+        while (!players.isEmpty()) {
+            System.out.println("Eleman çıkarılıyor: " + players.poll());
+        }
+
     }
+}
+
+class Player implements Comparable<Player> {
+    private String isim;
+    private int id;
+
+    public Player(String isim, int id) {
+        this.isim = isim;
+        this.id = id;
+    }
+
+    public String getIsim() {
+        return isim;
+    }
+
+    public void setIsim(String isim) {
+        this.isim = isim;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public int compareTo(Player player) {
+        if (this.id < player.id) {
+            return -1;
+        } else if (this.id > player.id) {
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Player [isim=" + isim + ", id=" + id + "]";
+    }
+
 }
