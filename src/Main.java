2cdb83438a65705f92f9330e8a99e476e740c465
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-08-04 22:23:33 +0300

    INPUT / OUTPUT TANIMLAR

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,40 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
-
 public class Main {
     public static void main(String[] args) {
 
-        // LISTITERATOR AND ITERATOR
+        // I / O
+
+        // Stream: verilerin input output yoluyla java projelerine aktığı bir veri
+        // dizisidir.
 
         /*
-         * Listiterator, SADECE List interfacesi implement eden classlarda kullanılır.
-         * next() ve previous() metotları vardır.
-         * ekstradan add() metotu bulunuur.
-         * 
-         * Iterator, set, queue, ve list interfacesi implement eden classlarda
-         * kullanılır.
-         * previous() metotu yoktur.
+         * System.out: Standart output stream.
+         * System.err: Standart error stream.
+         * System.in: Standart input stream.
          */
 
-        Set<String> set = new HashSet<>();
-        List<String> list = new ArrayList<>();
-        set.add("Java");
-        set.add("Python");
-        set.add("C++");
-        set.add("Go");
+        /*
+         * OutputStream
+         * Java uygulamaları bir dosyaya, bir sockete ve bağlanmış bir aygıta veriyi
+         * stream halinde yazmak için OutputStream abstract classından türeyen bir alt
+         * sınıfı kullanır.
+         * 
+         * write() metodu: Belli bir btye dizisini veya tek bir byte'ı hedefe yazar.
+         * flush() metodu: Herhangi bir bufferlanmış veri varsa bu metot sayesinde hemen
+         * hedefe yazılmasını söylemiş oluyoruz.
+         * close() metodu: OutpuStream'i kapatır.
+         */
 
-        list.add("Java");
-        list.add("Python");
-        list.add("C++");
-        list.add("Go");
-
-        Iterator<String> iterator1 = set.iterator();
-        Iterator<String> iterator2 = list.iterator();
-        while (iterator1.hasNext()) {
-            System.out.println(iterator1.next());
-        }
-
-        System.out.println("*********************");
-
-        while (iterator2.hasNext()) {
-            System.out.println(iterator2.next());
-        }
-
-        System.out.println("*****ITERATOR EN SONA GELDIGI ICIN BUNDAN SONRAKI WHILE DONGUSU CALISMIYOR*******");
-        // ITERATOR EN SONA GELDIGI ICIN BUNDAN SONRAKI WHILE DONGUSU CALISMIYOR
-        while (iterator2.hasNext()) {
-            System.out.println(iterator2.next());
-        }
-
-        System.out.println("*********************");
-
-        /*****************************************************/
-
-        List<String> list2 = new ArrayList<>();
-        list2.add("Java");
-        list2.add("Python");
-        list2.add("C++");
-        list2.add("Go");
-
-        ListIterator<String> listIterator = list.listIterator();
-        while (listIterator.hasNext()) {
-            System.out.println(listIterator.next());
-        }
-        // tersten yazdı
-        while (listIterator.hasPrevious()) {
-            System.out.println(listIterator.previous());
-        }
-
-        // "Go" üzerine gelince sil
-        while (listIterator.hasNext()) {
-            String value = listIterator.next();
-            if(value.equals("Go")) {
-                listIterator.remove();
-            }
-        }
+        /*
+         * InputStream
+         * Java uygulamaları bir dosyadan, bir socketten ve bağlanmış bir aygıttan
+         * veriyi Stream halinde okumak için InputStream absract classından türeyen bir
+         * alt sınıfı kullanır.
+         * 
+         * read() metodu: Kaynaktan bir sonraki byte'ı okur. Okuyacak herhangi bir byte
+         * yoksa -1 değeri döner.
+         * available() metodu: Okunabilecek byte sayısını döner.
+         * close() metodu: InputStream'i kapatır.
+         */
 
     }
 }
