c9f31497d6b66fa885aa0fde1d73de495ffe8429
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2023-08-05 01:10:53 +0300

    FILEWRITER

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,30 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
 
-        FileInputStream fis = null;
+        // FileWriter
+        // FileOutputStream kullanırken dönüştürme işlemi yapmak zorunda kalıyoruz.
+        // Bunun önüne geçer.
+
+        FileWriter fileWriter = null;
         try {
-            fis = new FileInputStream("dosya.txt");
-            /*
-             * // cast etmezsen ascii değerini getirir.
-             * 
-             * System.out.println((char) fis.read()); // J
-             * System.out.println((char) fis.read()); // a
-             * System.out.println((char) fis.read()); // v
-             * 
-             * // İmleç her seferinde ilerliyor.
-             */
-            /*
-             * fis.skip(5); // imleç 5 kez hareket eder.
-             * 
-             * System.out.println((char)fis.read()); // P
-             * System.out.println((char) fis.read()); // r
-             */
 
-            /*
-             * // Dosyanın tamamını okumak
-             * int deger;
-             * String s = "";
-             * 
-             * while ((deger = fis.read()) != -1) {
-             * s += (char) deger;
-             * }
-             * 
-             * System.out.println("Dosya içeriği: " + s);
-             */
+            fileWriter = new FileWriter("dosya.txt", true);
+            // fileWriter.write("Ahmet Samet Özalp\n");
 
-            int deger;
-            String s = "";
-            int say = 0;
-
-            fis.skip(5);
-            while ((deger = fis.read()) != -1) {
-                s += (char) deger;
-                say++;
-                if (say == 10) {
-                    break;
-                }
-            }
-            System.out.println("Dosyanın 5. yerinden itibaren 10 karakter: " + s);
-
-        } catch (FileNotFoundException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         } finally {
-            try {
-                if (fis != null)
-                    fis.close();
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            if (fileWriter != null)
+                try {
+                    fileWriter.close();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
         }
 
     }
